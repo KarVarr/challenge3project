0315ea267aac00b4dfdbb7f7f8566236c4ee9406
KarVarr <megatr9n@gmail.com>
2022-12-10 23:01:54 +0300

    Initial Commit

---
+++ challenge3project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge3project
+//
+//  Created by Karen Vardanian on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
