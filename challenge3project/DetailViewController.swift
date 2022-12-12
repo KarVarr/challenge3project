3cbf2381ea475a880206e2fc47e810543053ddb0
KarVarr <megatr9n@gmail.com>
2022-12-12 18:27:25 +0300

    added detailViewController

---
+++ challenge3project/DetailViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetailViewController.swift
+//  challenge3project
+//
+//  Created by Karen Vardanian on 12.12.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+   @objc func shareTapped()  {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1)  else {
+            return
+        }
+    }
+    
+    var imageName:[Any] = [image]
+    if let imageText = selectedImage {
+        imageName.append(imageText)
+    }
+    
+    let vc = UIActivityViewController(activityItems: [image, "Name image: \(imageName)"], applicationActivities: [])
+    vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+    present(vc, animated: true)
+    
+
+}
--- challenge3project/TableViewController.swift
+++ challenge3project/TableViewController.swift
@@ -14,6 +14,10 @@ var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Flag"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -35,16 +39,17 @@ var pictures = [String]()
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
             cell.textLabel?.text = "Picture \(pictures.firstIndex(of: pictures[indexPath.row])! + 1) of \(pictures.count)"
+        
             return cell
         }
         
-//        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//            if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-//                vc.selectedImage = pictures[indexPath.row]
-//
-//                navigationController?.pushViewController(vc, animated: true)
-//            }
-//        }
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+                vc.selectedImage = pictures[indexPath.row]
+
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        }
     
     
 
